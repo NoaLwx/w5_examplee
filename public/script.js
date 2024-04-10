236230072565b0dbee05833b222a8a43d9e17aba
NoaLwx <130018250+NoaLwx@users.noreply.github.com>
2024-04-10 10:22:22 +1000

    index

--- public/script.js
+++ public/script.js
@@ -1,14 +1,19 @@
 document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
 
+//remember to put document.getElementById to get the element
+//whatever on the right handside is assigned to the left handside
 const cnv = document.getElementById (`cnv_element`)
 cnv.width = innerWidth
 cnv.height = innerHeight
 
+//context is the thing that draw things and get data from the canvas
 const ctx = cnv.getContext (`2d`)
 
+//addressing the Context
 const draw_frame = () => {
    ctx.fillStyle = `turquoise`
+   //the same as background in p5
    ctx.fillRect (0, 0, innerWidth, innerHeight)
 
    requestAnimationFrame (draw_frame)
@@ -16,6 +21,7 @@ const draw_frame = () => {
 
 draw_frame ()
 
+//whenever you resixe the window, it resize the canvas
 window.onresize = () => {
    cnv.width = innerWidth
    cnv.height = innerHeight   
